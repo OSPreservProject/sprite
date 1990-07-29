51ee2c4adb1b960d807b9dce59f685882f09c543
shirriff <>
1990-07-29 15:51:38 +0000

    Moved stuff to procServer.h.

--- CVSROOT/kernel/proc/procServer.c
+++ CVSROOT/kernel/proc/procServer.c
@@ -32,36 +32,7 @@ static char rcsid[] = "$Header$ SPRITE (Berkeley)";
 #include "string.h"
 #include "status.h"
 #include "stdlib.h"
-
-/*
- * Information kept for each function that is scheduled to be called in the
- * future.
- */
-typedef struct {
-    void		(*func)();	/* Function to call. */
-    ClientData		data;		/* Data to pass to function. */
-    Boolean		allocated;	/* TRUE => Struct was allocated by
-					 *         malloc. */
-    Timer_QueueElement	queueElement;	/* Element used to put onto timer
-					 * queue. */
-} FuncInfo;
-
-/*
- * Element of queue of pending requests for functions to be called.
- */
-typedef struct {
-    void	(*func)();		/* Function to call. */
-    ClientData	data;			/* Data to pass to function. */
-    FuncInfo	*funcInfoPtr;		/* Pointer to function info struct
-					 * that was allocated if were
-					 * put onto timer queue. */
-} QueueElement;
-
-/*
- * NUM_QUEUE_ELEMENTS	Maximum number of entries in the queue of pending
- *			functions.
- */
-#define	NUM_QUEUE_ELEMENTS	128
+#include "procServer.h"
 
 /*
  * Circular queue of pending function calls.
@@ -75,54 +46,16 @@ QueueElement	queue[NUM_QUEUE_ELEMENTS];
  */
 int	frontIndex = -1;
 int	nextIndex = 0;
-#define	QUEUE_EMPTY	(frontIndex == -1)
-#define	QUEUE_FULL	(frontIndex == nextIndex)
 
-/*
- * Information kept for each server process.
- */
-typedef struct {
-    int			index;
-    int			flags;	/* Flags defined below. */
-    QueueElement	info;	/* Information to indicate next function to
-				 * execute. */
-    Sync_Condition	condition;	/* Condition to sleep on when waiting
-					 * for something to do. */
-} ServerInfo;
 ServerInfo	*serverInfoTable;
 
-/*
- * Flags for server info struct:
- *
- *	ENTRY_INUSE	There is a server process associated with this
- *			entry.
- *	SERVER_BUSY	The server is busy executing some command.
- *	FUNC_PENDING	There is a function to execute.
- */
-#define	ENTRY_INUSE	0x1
-#define	SERVER_BUSY	0x2
-#define	FUNC_PENDING	0x4
-
-/*
- * Number of server processes.  There have to be enough to allow for
- * pageouts and block cleaning at the same time. This occurs while
- * paging heavily on a file server (or with a local disk used for paging).
- */
-#define PROC_NUM_SERVER_PROCS	(FSCACHE_MAX_CLEANER_PROCS + VM_MAX_PAGE_OUT_PROCS)
 int	proc_NumServers = PROC_NUM_SERVER_PROCS;
 
 /* 
- * Mutex to synchronize accsess to the queue of pending requests and
+ * Mutex to synchronize accesses to the queue of pending requests and
  * to the process state.
  */
 volatile Sync_Semaphore	serverMutex; 
-
-
-
-extern void	Proc_ServerProc();
-static void	ScheduleFunc();
-static void	CallFunc();
-static void	CallFuncFromTimer();
 
 
 /*
