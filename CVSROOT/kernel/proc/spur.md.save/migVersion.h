dad776601c981628aa9e67bf5bb14ebbb93cd022
douglis <>
1990-07-30 16:16:03 +0000

    incremented migration version

--- CVSROOT/kernel/proc/spur.md.save/migVersion.h
+++ CVSROOT/kernel/proc/spur.md.save/migVersion.h
@@ -20,6 +20,6 @@
  * machines of the same architecture and version number.
  */
 #ifndef PROC_MIGRATE_VERSION
-#define PROC_MIGRATE_VERSION 10
+#define PROC_MIGRATE_VERSION 11
 #endif /* PROC_MIGRATE_VERSION */
 
