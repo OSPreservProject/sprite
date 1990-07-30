7286833120a440df2633cc64315f3282df54fe3c
douglis <>
1990-07-30 16:03:27 +0000

    incremented migration version.

--- CVSROOT/kernel/proc/ds3100.md/migVersion.h
+++ CVSROOT/kernel/proc/ds3100.md/migVersion.h
@@ -20,6 +20,6 @@
  * machines of the same architecture and version number.
  */
 #ifndef PROC_MIGRATE_VERSION
-#define PROC_MIGRATE_VERSION 14
+#define PROC_MIGRATE_VERSION 15
 #endif /* PROC_MIGRATE_VERSION */
 
--- CVSROOT/kernel/proc/sun4.md/migVersion.h
+++ CVSROOT/kernel/proc/sun4.md/migVersion.h
@@ -20,6 +20,6 @@
  * machines of the same architecture and version number.
  */
 #ifndef PROC_MIGRATE_VERSION
-#define PROC_MIGRATE_VERSION 1014
+#define PROC_MIGRATE_VERSION 1015
 #endif /* PROC_MIGRATE_VERSION */
 
