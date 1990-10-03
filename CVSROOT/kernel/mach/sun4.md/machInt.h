196d88f23fbc57209b4444085cc4bfb3e3fcdc06
mgbaker <>
1990-10-03 14:15:42 +0000

    this is JohnH checking it in for mary.  Looks like she was doing prototyping.

--- CVSROOT/kernel/mach/sun4.md/machInt.h
+++ CVSROOT/kernel/mach/sun4.md/machInt.h
@@ -19,15 +19,26 @@
 #ifndef _MACHINT
 #define _MACHINT
 
+#include "sys/types.h"
+#ifdef KERNEL
 #include "mach.h"
+#else
+#include <kernel/mach.h>
+#endif
+
 /*
  * The bounds of the code that copies arguments from the user stack to
  * the kernel stack.
  */
-extern int MachFetchArgs();
-extern int MachFetchArgsEnd();
-extern Address Mach_ProbeStart;
-extern Address Mach_ProbeEnd;
+extern Address MachFetchArgs;
+extern Address MachFetchArgsEnd;
+extern Address MachProbeStart;
+extern Address MachProbeEnd;
+
+extern void MachPageFault _ARGS_((unsigned int busErrorReg, Address addrErrorReg, unsigned int trapPsr, Address pcValue));
+extern int MachUserAction _ARGS_((void));
+extern void MachHandleTrap _ARGS_((int trapType, Address pcValue, unsigned int trapPsr));
+extern void MachUserDebug _ARGS_((void));
 
 /*
  * Asm routines.
@@ -45,8 +56,9 @@ extern void MachFPU_Emulate _ARGS_((int processID, Address instAddr,
 extern void MachFPUDumpState _ARGS_((Mach_RegState *regStatePtr));
 extern void MachFPULoadState _ARGS_((Mach_RegState *regStatePtr));
 
-extern	void		Mach_FreeState();
-extern	ReturnStatus	Mach_SetupNewState();
-extern void Mach_MonTrap();
+extern void MachCallSigReturn _ARGS_((void));
+extern int fuword _ARGS_((caddr_t address));
+extern int fubyte _ARGS_((caddr_t address));
+extern int suword _ARGS_((caddr_t address, int value));
 
 #endif /* _MACHINT */
