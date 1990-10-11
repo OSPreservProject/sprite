aa6adb746e2a7104bc21bcac349994e6bcc6bddb
kupfer <>
1990-10-11 21:59:59 +0000

    Use <> include's.  Change eventTable to get rid of casts and compiler warnings.

--- CVSROOT/kernel/utils/dump.c
+++ CVSROOT/kernel/utils/dump.c
@@ -17,35 +17,37 @@ static char rcsid[] = "$Header$ SPRITE (Berkeley)";
 #endif /* not lint */
 
 
-#include "net.h"
-#include "sprite.h"
-#include "mach.h"
-#include "dump.h"
-#include "dumpInt.h"
-#include "timer.h"
-#include "proc.h"
-#include "sys.h"
-#include "list.h"
-#include "devVid.h"
-#include "rpc.h"
-#include "fs.h"
-#include "fsutil.h"
-#include "fscache.h"
-#include "fspdev.h"
-#include "recov.h"
-#include "string.h"
-#include "sched.h"
-#include "devSyslog.h"
-#include "mem.h"
+#include <net.h>
+#include <sprite.h>
+#include <mach.h>
+#include <dump.h>
+#include <dumpInt.h>
+#include <timer.h>
+#include <proc.h>
+#include <sys.h>
+#include <list.h>
+#include <devVid.h>
+#include <rpc.h>
+#include <fs.h>
+#include <fsutil.h>
+#include <fscache.h>
+#include <fspdev.h>
+#include <recov.h>
+#include <string.h>
+#include <sched.h>
+#include <devSyslog.h>
+#include <mem.h>
 #include <stdio.h>
 
 /*
  * Forward references to procedures internal to this file.
  */
+static void	DumpProcTable _ARGS_((ClientData));
 static void	PrintL1Menu _ARGS_((ClientData));
 static void	PrintTOD _ARGS_((ClientData));
 static void	PrintVersion _ARGS_((ClientData));
 static void	ResetNetworks _ARGS_((ClientData));
+static void	VidEnable _ARGS_((ClientData));
 
 /*
  * Table of routines and their arguments to be called on dump events.
@@ -53,7 +55,7 @@ static void	ResetNetworks _ARGS_((ClientData));
  * Machine dependent event should be added to the Local event table.
  */
 static EventTableType eventTable[] = {
-    {'/', PrintL1Menu, (ClientData)0,"Print Dump Event Menu"},
+    {'/', PrintL1Menu, (ClientData)0, "Print Dump Event Menu"},
     {'a', RESERVED_EVENT, NULL_ARG, "Abort to PROM monitor" }, 
 #ifdef notdef
     {'b', RESERVED_EVENT, NULL_ARG, 
@@ -62,22 +64,23 @@ static EventTableType eventTable[] = {
     {'c', Fscache_DumpStats, (ClientData)0, "Dump cache stats"},
     {'d', RESERVED_EVENT, NULL_ARG, "Put machine into the kernel debugger"},
     {'e', Timer_DumpStats, (ClientData) 'e', "Dump timer stats"},
-    {'f', Fsutil_PrintTrace,   (ClientData) -1, "Dump filesystem trace"},
-    {'i', (void (*)()) Proc_KDump, (ClientData) 0,"Info on waiting processes"},
-    {'j', Dev_SyslogDisable, (ClientData) 0,"Disable/enable syslog"},
-    {'m', Mem_DumpStats, (ClientData) FALSE,"Dump memory stats"},
-    {'n', ResetNetworks, (ClientData)0,"Reset the network interfaces"},
-    {'o', (void (*)()) Dev_VidEnable, (ClientData) 1,"Turn video on"},
-    {'p', (void (*)()) Proc_Dump, (ClientData) 0,"Dump process table"},
-    {'r', Sched_DumpReadyQueue,  (ClientData) 0,"Dump ready queue"},
-    {'q', Fspdev_PrintTrace,  (ClientData) 200,"Dump pseudo-device trace"},
-    {'s', Timer_DumpStats,   (ClientData) 's',"Reset timer stats"},
-    {'t', Timer_DumpQueue,  (ClientData) 0,"Dump the timer queue"},
-    {'v', PrintVersion, (ClientData) 0,"Print version string of the kernel"},
+    {'f', Fsutil_PrintTrace, (ClientData) -1, "Dump filesystem trace"},
+    {'i', Proc_KDump, (ClientData) 0, "Info on waiting processes"},
+    {'j', Dev_SyslogDisable, (ClientData) 0, "Disable/enable syslog"},
+    {'m', Mem_DumpStats, (ClientData) FALSE, "Dump memory stats"},
+    {'n', ResetNetworks, (ClientData)0, "Reset the network interfaces"},
+    {'o', VidEnable, (ClientData) 1, "Turn video on"},
+    {'p', DumpProcTable, (ClientData) 0, "Dump process table"},
+    {'r', Sched_DumpReadyQueue, (ClientData) 0, "Dump ready queue"},
+    {'q', Fspdev_PrintTrace, (ClientData) 200, "Dump pseudo-device trace"},
+    {'s', Timer_DumpStats,   (ClientData) 's', "Reset timer stats"},
+    {'t', Timer_DumpQueue,  (ClientData) 0, "Dump the timer queue"},
+    {'v', PrintVersion, (ClientData) 0, "Print version string of the kernel"},
     {'w', Fsutil_SyncStub, (ClientData) FALSE, "WRITE BACK CACHE"},
-    {'x', Fsutil_HandleScavengeStub, (ClientData) 0,"Scavenge filesystem handles"},
-    {'y', Recov_PrintTrace, (ClientData) 50,"Dump RPC recovery trace"},
-    {'z', Rpc_PrintTrace, (ClientData) 50,"Dump RPC packet trace"},
+    {'x', Fsutil_HandleScavengeStub, (ClientData) 0, 
+					"Scavenge filesystem handles"},
+    {'y', Recov_PrintTrace, (ClientData) 50, "Dump RPC recovery trace"},
+    {'z', Rpc_PrintTrace, (ClientData) 50, "Dump RPC packet trace"},
     {'1', Timer_TimerGetInfo, (ClientData) 1,"Dump info for timer counter 1"},
     {'2', Timer_TimerGetInfo, (ClientData) 2,"Dump info for timer counter 2"},
     {'3', Timer_TimerGetInfo, (ClientData) 3,"Dump info for timer counter 3"},
@@ -88,41 +91,6 @@ static EventTableType eventTable[] = {
     {'\0', LAST_EVENT,        NULL_ARG,       (char *) 0 },
 };
 
-
-
-/*
- *----------------------------------------------------------------------
- *
- * PrintL1Menu --
- *
- *	Dump out a list of the L1-key magic commands.
- *
- * Results:
- *	None.
- *
- * Side effects:
- *	None.
- *
- *----------------------------------------------------------------------
- */
-
-static void
-PrintL1Menu(arg)
-    ClientData arg;		/* unused */
-{
-    EventTableType	*entry;
-
-    printf("Pressing the L1 key and a letter causes the following...\n");
-    printf("/ or ? - Print this menu\n");
-    for (entry = eventTable; entry->routine != LAST_EVENT; entry++) {
-	printf("%c - %s\n",entry->key, entry->description);
-    }
-    /*
-     * Show the machine dependent bindings.
-     */
-    printf("Machine dependent entries....\n");
-    Dump_Show_Local_Menu();
-}
 
 
 /*
@@ -147,6 +115,67 @@ Dump_Init()
     Dump_Register_Events(eventTable);
 
 }
+
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * DumpProcTable --
+ *
+ *	Wrapper to avoid casts and compiler warnings.
+ *
+ * Results:
+ *	None.
+ *
+ * Side effects:
+ *	(see Proc_Dump.)
+ *
+ *----------------------------------------------------------------------
+ */
+
+/* ARGSUSED */
+static void
+DumpProcTable(dummy)
+    ClientData dummy;
+{
+    Proc_Dump();
+}
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * PrintL1Menu --
+ *
+ *	Dump out a list of the L1-key magic commands.
+ *
+ * Results:
+ *	None.
+ *
+ * Side effects:
+ *	None.
+ *
+ *----------------------------------------------------------------------
+ */
+
+/* ARGSUSED */
+static void
+PrintL1Menu(arg)
+    ClientData arg;		/* unused */
+{
+    EventTableType	*entry;
+
+    printf("Pressing the L1 key and a letter causes the following...\n");
+    printf("/ or ? - Print this menu\n");
+    for (entry = eventTable; entry->routine != LAST_EVENT; entry++) {
+	printf("%c - %s\n",entry->key, entry->description);
+    }
+    /*
+     * Show the machine dependent bindings.
+     */
+    printf("Machine dependent entries....\n");
+    Dump_Show_Local_Menu();
+}
+
 
 /*
  *----------------------------------------------------------------------
@@ -245,3 +274,26 @@ ResetNetworks(arg)
     }
 }
 
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * VidEnable --
+ *
+ *	Wrapper to avoid casts and compiler warnings.
+ *
+ * Results:
+ *	None.
+ *
+ * Side effects:
+ *	(see Dev_VidEnable.)
+ *
+ *----------------------------------------------------------------------
+ */
+
+static void
+VidEnable(onOff)
+    ClientData onOff;
+{
+    Dev_VidEnable((Boolean)onOff);
+}
