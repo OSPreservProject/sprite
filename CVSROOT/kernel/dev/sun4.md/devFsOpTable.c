1a09c00fd63d28a2e37a1b2ef42cd6d11d2670a2
mgbaker <>
1990-10-19 15:39:47 +0000

    Added mmap switch entries. Somehow they must have gotten deleted before?

--- CVSROOT/kernel/dev/sun4.md/devFsOpTable.c
+++ CVSROOT/kernel/dev/sun4.md/devFsOpTable.c
@@ -42,6 +42,7 @@ static char rcsid[] = "$Header$ SPRITE (Berkeley)";
 #include "tty.h"
 #include "mouse.h"
 #include "devTMR.h"
+#include "devfb.h"
 
 static ReturnStatus NoDevice();
 static ReturnStatus NullProc();
@@ -58,6 +59,7 @@ static ReturnStatus NullProc();
  *	DeviceSelect
  *	BlockDeviceAttach
  *	DeviceReopen
+ *	DeviceMmap
  */
 
 
@@ -67,84 +69,90 @@ DevFsTypeOps devFsOpTable[] = {
      */
     {DEV_TERM,       DevTtyOpen, DevTtyRead, DevTtyWrite,
 		     DevTtyIOControl, DevTtyClose, DevTtySelect,
-		     DEV_NO_ATTACH_PROC, NoDevice},
+		     DEV_NO_ATTACH_PROC, NoDevice, NullProc},
     /*
      * The system error log.  If this is not open then error messages go
      * to the console.
      */
     {DEV_SYSLOG,    Dev_SyslogOpen, Dev_SyslogRead, Dev_SyslogWrite,
 		    Dev_SyslogIOControl, Dev_SyslogClose, Dev_SyslogSelect,
-		    DEV_NO_ATTACH_PROC, Dev_SyslogReopen},
+		    DEV_NO_ATTACH_PROC, Dev_SyslogReopen, NullProc},
     /*
      * SCSI Worm interface:  this device doesn't exist anymore.
      */
     {DEV_SCSI_WORM, Dev_TimerOpen, Dev_TimerRead, NullProc,
                     Dev_TimerIOControl, NullProc, NullProc,
-                    DEV_NO_ATTACH_PROC, NoDevice},
+                    DEV_NO_ATTACH_PROC, NoDevice, NullProc},
     /*
      * The following device number is unused.
      */
     {DEV_PLACEHOLDER_2, NoDevice, NullProc, NullProc,
 		    Dev_NullIOControl, NullProc, NullProc,
-		    DEV_NO_ATTACH_PROC, NoDevice},
+		    DEV_NO_ATTACH_PROC, NoDevice, NullProc},
     /*
      * New SCSI Disk interface.
      */
     {DEV_SCSI_DISK, DevRawBlockDevOpen, DevRawBlockDevRead,
 		    DevRawBlockDevWrite, DevRawBlockDevIOControl, 
 		    DevRawBlockDevClose, NullProc, DevScsiDiskAttach,
-		    DevRawBlockDevReopen},
+		    DevRawBlockDevReopen, NullProc},
     /*
      * SCSI Tape interface.
      */
     {DEV_SCSI_TAPE, DevSCSITapeOpen, DevSCSITapeRead, DevSCSITapeWrite,
 		    DevSCSITapeIOControl, DevSCSITapeClose, NullProc,
-		    DEV_NO_ATTACH_PROC, NoDevice},
+		    DEV_NO_ATTACH_PROC, NoDevice, NullProc},
     /*
      * /dev/null
      */
     {DEV_MEMORY,    NullProc, Dev_NullRead, Dev_NullWrite,
 		    Dev_NullIOControl, NullProc, Dev_NullSelect,
-		    DEV_NO_ATTACH_PROC, NullProc},
+		    DEV_NO_ATTACH_PROC, NullProc, NullProc},
     /*
      * Xylogics 450 disk controller.
      */
     {DEV_XYLOGICS, DevRawBlockDevOpen, DevRawBlockDevRead,
 		    DevRawBlockDevWrite, DevRawBlockDevIOControl, 
 		    DevRawBlockDevClose, NullProc, DevXylogics450DiskAttach,
-		    DevRawBlockDevReopen},
+		    DevRawBlockDevReopen, NullProc},
     /*
      * Network devices.  The unit number specifies the ethernet protocol number.
      */
     {DEV_NET,      DevNet_FsOpen, DevNet_FsRead, DevNet_FsWrite, 
 		   DevNet_FsIOControl, DevNet_FsClose, DevNet_FsSelect, 
-		   DEV_NO_ATTACH_PROC, DevNet_FsReopen},
+		   DEV_NO_ATTACH_PROC, DevNet_FsReopen, NullProc},
     /*
      * Raw SCSI HBA interface.
      */
     {DEV_SCSI_HBA, DevSCSIDeviceOpen, Dev_NullRead, Dev_NullWrite,
 		    DevSCSIDeviceIOControl, DevSCSIDeviceClose, Dev_NullSelect,
-		    DEV_NO_ATTACH_PROC, NoDevice},
+		    DEV_NO_ATTACH_PROC, NoDevice, NullProc},
     /*  
      * RAID device.
      */ 
     {DEV_RAID, DevRawBlockDevOpen, DevRawBlockDevRead,
                     DevRawBlockDevWrite, DevRawBlockDevIOControl,
                     DevRawBlockDevClose, NullProc, DevRaidAttach,
-                    DevRawBlockDevReopen},
+                    DevRawBlockDevReopen, NullProc},
     /*  
      * Debug device. (useful for debugging RAID device)
      */ 
     {DEV_DEBUG, DevRawBlockDevOpen, DevRawBlockDevRead,
                     DevRawBlockDevWrite, DevRawBlockDevIOControl,
                     DevRawBlockDevClose, NullProc, DevDebugAttach,
-                    DevRawBlockDevReopen},
+                    DevRawBlockDevReopen, NullProc},
     /*
      * Event devices for window systems.
      */
     {DEV_MOUSE,    DevMouseOpen, DevMouseRead, DevMouseWrite,
 		   DevMouseIOControl, DevMouseClose, DevMouseSelect,
-		   DEV_NO_ATTACH_PROC, NoDevice},
+		   DEV_NO_ATTACH_PROC, NoDevice, NullProc},
+    /*
+     * Frame buffer device.
+     */
+    {DEV_GRAPHICS, DevFBOpen, NullProc, NullProc,
+                   DevFBIOControl, DevFBClose, NullProc,
+                   DEV_NO_ATTACH_PROC, NoDevice, DevFBMMap},
 
 };
 
