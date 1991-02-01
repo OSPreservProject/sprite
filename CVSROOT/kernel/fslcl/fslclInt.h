e3793f40d58329eb8837b87a565c3060498acced
shirriff <>
1991-02-01 16:32:04 +0000

    Added clientID to attribute tracing.

--- CVSROOT/kernel/fslcl/fslclInt.h
+++ CVSROOT/kernel/fslcl/fslclInt.h
@@ -77,10 +77,18 @@ extern ReturnStatus FslclHardLink _ARGS_((Fs_HandleHeader *prefixHandle1,
  * Declarations for the Local Domain attribute operations called via
  * the fsAttrOpsTable switch.  These are called with a fileID.
  */
+#ifdef SOSP91
+extern ReturnStatus FslclGetAttr _ARGS_((Fs_FileID *fileIDPtr, int clientID, 
+		Fs_Attributes *attrPtr, int hostID, int userID));
+extern ReturnStatus FslclSetAttr _ARGS_((Fs_FileID *fileIDPtr, 
+		Fs_Attributes *attrPtr, Fs_UserIDs *idPtr, int flags, int
+		clientID, int hostID, int userID));
+#else
 extern ReturnStatus FslclGetAttr _ARGS_((Fs_FileID *fileIDPtr, int clientID, 
 		Fs_Attributes *attrPtr));
 extern ReturnStatus FslclSetAttr _ARGS_((Fs_FileID *fileIDPtr, 
 		Fs_Attributes *attrPtr, Fs_UserIDs *idPtr, int flags));
+#endif
 
 extern void FslclAssignAttrs _ARGS_((Fsio_FileIOHandle *handlePtr,
 		Boolean isExeced, Fs_Attributes *attrPtr));
