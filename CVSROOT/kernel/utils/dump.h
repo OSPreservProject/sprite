e3770c1dac848f4fd4ad9d34bffd9c07c1df6e3c
kupfer <>
1991-05-17 10:37:38 +0000

    Add DUMP_LINES_PER_SCREEN.

--- CVSROOT/kernel/utils/dump.h
+++ CVSROOT/kernel/utils/dump.h
@@ -13,6 +13,14 @@
 #ifndef _DUMP
 #define _DUMP
 
+/* 
+ * For L1 routines that want to print a header line every screenful, 
+ * this #define gives a common definition for how big a screen is.  A 
+ * few extra lines are provided so that, e.g., syslog output is less 
+ * likely to make the header scroll off the screen.
+ */
+#define DUMP_LINES_PER_SCREEN	20
+
 extern void	Dump_Init _ARGS_((void));
 
 #endif /* _DUMP */
