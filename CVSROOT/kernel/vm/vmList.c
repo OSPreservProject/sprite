979bb7ff9d3fcbb4bf53500386ffa562a63a4507
jhh <>
1991-06-21 12:07:11 +0000

    added copyright to top of file

--- CVSROOT/kernel/vm/vmList.c
+++ CVSROOT/kernel/vm/vmList.c
@@ -1,3 +1,22 @@
+/* 
+ * vmList.c --
+ *
+ *	Routines for maintaining lists
+ *
+ * Copyright 1991 Regents of the University of California
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that this copyright
+ * notice appears in all copies.  The University of California
+ * makes no representations about the suitability of this
+ * software for any purpose.  It is provided "as is" without
+ * express or implied warranty.
+ */
+
+#ifndef lint
+static char rcsid[] = "$Header$ SPRITE (Berkeley)";
+#endif /* not lint */
+
 #include <sprite.h>
 #include <list.h>
 #include <sys.h>
