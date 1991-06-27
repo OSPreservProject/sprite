0f1055467ba1bae969ad7bc022b2f1aaee0b6292
shirriff <>
1991-06-27 12:16:32 +0000

    Added copyright.

--- CVSROOT/kernel/utils/trace.c
+++ CVSROOT/kernel/utils/trace.c
@@ -8,8 +8,14 @@
  *	A circular buffer of trace records can be dumped to a file via calls
  *	to Trace_Dump.
  *
- * Copyright (C) 1986 Regents of the University of California
- * All rights reserved.
+ * Copyright 1986 Regents of the University of California
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that this copyright
+ * notice appears in all copies.  The University of California
+ * makes no representations about the suitability of this
+ * software for any purpose.  It is provided "as is" without
+ * express or implied warranty.
  */
 
 #ifndef lint
--- CVSROOT/kernel/utils/trace.h
+++ CVSROOT/kernel/utils/trace.h
@@ -3,9 +3,14 @@
  *
  *	Definitions for the generalized tracing facility.
  *
- * Copyright (C) 1986 Regents of the University of California
- * All rights reserved.
- *
+ * Copyright 1991 Regents of the University of California
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that this copyright
+ * notice appears in all copies.  The University of California
+ * makes no representations about the suitability of this
+ * software for any purpose.  It is provided "as is" without
+ * express or implied warranty.
  *
  * $Header$ SPRITE (Berkeley)
  */
