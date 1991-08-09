af9aa66ba9f41545d256ada6da7cfae15ab5d874
mgbaker <>
1991-08-09 16:22:27 +0000

    New version so that sparc2's and 1's can migrate to each other.

--- CVSROOT/kernel/proc/sun4c.md/migVersion.h
+++ CVSROOT/kernel/proc/sun4c.md/migVersion.h
@@ -20,6 +20,6 @@
  * machines of the same architecture and version number.
  */
 #ifndef PROC_MIGRATE_VERSION
-#define PROC_MIGRATE_VERSION 18
+#define PROC_MIGRATE_VERSION 19
 #endif /* PROC_MIGRATE_VERSION */
 
