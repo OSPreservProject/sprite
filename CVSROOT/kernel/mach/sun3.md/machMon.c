c95db5b21b10383321015f3259a5db763c7a0cec
dlong <>
1991-08-14 14:20:58 +0000

    added code to traverse device tree for sun4c

--- CVSROOT/kernel/mach/sun3.md/machMon.c
+++ CVSROOT/kernel/mach/sun3.md/machMon.c
@@ -19,6 +19,10 @@ static char rcsid[] = "$Header$ SPRITE (Berkeley)";
 #include "mach.h"
 #include "vmMach.h"
 #include "sys.h"
+#ifdef sun4c
+#include "stdio.h"
+#include "string.h"
+#endif
 
 #ifdef sun2
 static	int	(*savedNmiVec)() = (int (*)()) 0;
@@ -241,3 +245,164 @@ Mach_MonStopNmi()
 #endif
     }
 }
+
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * Mach_MonTraverseDevTree --
+ *
+ *	Traverse the device tree, and call the given function
+ *	for each node found.  To start at the root of the tree,
+ *	the node argument should be set to 0.
+ *
+ * Results:
+ *	None.
+ *
+ *----------------------------------------------------------------------
+ */
+#ifdef sun4c
+void
+Mach_MonTraverseDevTree(node, func, clientData)
+    unsigned	int	node;
+    int		(*func)();
+    void	*clientData;
+{
+    unsigned	int		newNode;
+    char 	name[64];
+    int		length;
+    struct	config_ops	*configPtr;
+
+    configPtr = romVectorPtr->v_config_ops;
+    if (node == 0) {
+	node = configPtr->devr_next(0);
+    }
+    while (node != 0) {
+	length = configPtr->devr_getproplen(node, "name");
+	if (length > 0) {
+	    if (length > sizeof (name)) {
+		panic("Mach_MonTraverseDevTree: buffer too small.\n");
+	    }
+	    configPtr->devr_getprop(node, "name", name);
+	    if ((*func)(node, name, clientData))
+		return;
+	}
+	if (newNode = configPtr->devr_child(node)) {
+	    Mach_MonTraverseDevTree(newNode, func, clientData);
+	}
+	node = configPtr->devr_next(node);
+    }
+}
+
+
+static int
+PrintNode(node, name, clientData)
+    unsigned	int	node;
+    char		*name;
+    void		*clientData;
+{
+    struct	config_ops	*configPtr;
+    char	*prop;
+
+    configPtr = romVectorPtr->v_config_ops;
+    prop = 0;
+    while (1) {
+	prop = (char *)configPtr->devr_nextprop(node, prop);
+	if (prop && *prop) {
+	    printf("%s: %s\n", name, prop);
+	} else {
+	    break;
+	}
+    }
+    return 0;
+}
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * Mach_MonTraverseAndPrintDevTree --
+ *
+ *	Traverse the device tree, and display the attributes
+ *	of each node found.
+ *
+ * Results:
+ *	None.
+ *
+ *----------------------------------------------------------------------
+ */
+
+void
+Mach_MonTraverseAndPrintDevTree()
+{
+    Mach_MonTraverseDevTree(0, PrintNode, 0);
+}
+
+
+struct ConfigBuf {
+    char	*name;
+    char	*attr;
+    char	*buf;
+    int		buflen;
+    int		length;
+};
+
+static int
+CheckNode(node, name, clientData)
+    unsigned	int		node;
+    char			*name;
+    struct	ConfigBuf	*clientData;
+{
+    int		length;
+    struct	config_ops	*configPtr;
+
+    configPtr = romVectorPtr->v_config_ops;
+    if (strcmp(name, clientData->name) == 0
+	    || strcmp(clientData->name, "*") == 0) {
+	length = configPtr->devr_getproplen(node, clientData->attr);
+	if (length <= 0) {
+	    return 0;
+	} else if (length > clientData->buflen) {
+	    printf("Data size (%d) is greater than buffer size (%d)\n",
+		length, clientData->buflen);
+	} else {
+	    configPtr->devr_getprop(node, clientData->attr, clientData->buf);
+	}
+	clientData->length = length;
+	return 1;
+    }
+    return 0;
+}
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * Mach_MonSearchProm --
+ *
+ *	Search through the prom devices to find out system attributes.
+ *	If attr is given, and name is "*", the info will be retrieved
+ *	from the first node with that attribute, otherwise the name
+ *	must match exactly.
+ *
+ * Results:
+ *	length of info retrieved.
+ *
+ *----------------------------------------------------------------------
+ */
+int
+Mach_MonSearchProm(name, attr, buf, buflen)
+    char	*name;
+    char	*attr;
+    char	*buf;
+    int		buflen;
+{
+    struct ConfigBuf data;
+
+    data.length = 0;
+    data.name = name;
+    data.attr = attr;
+    data.buf = buf;
+    data.buflen = buflen;
+    Mach_MonTraverseDevTree(0, CheckNode, &data);
+    return data.length;
+}
+#endif /* sun4c */
