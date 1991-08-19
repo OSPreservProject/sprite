5d932b891488142b6edc21ad5a9139d64428ec11
mendel <>
1991-08-19 12:55:07 +0000

    Changed SCSI3 controller 0 to have a name consistent with the rest of the controllers.

--- CVSROOT/kernel/dev/sun4.md/devConfig.c
+++ CVSROOT/kernel/dev/sun4.md/devConfig.c
@@ -40,7 +40,7 @@ static char rcsid[] = "$Header$ SPRITE (Berkeley)";
  */
 DevConfigController devCntrlr[] = {
    /* Name     Address,  Addr space, ID, InitProc   IntrVector  IntrRoutine. */
-    { "SCSI3",  0x200000, DEV_VME_D16A24, 0, DevSCSI3Init, 64, DevSCSI3Intr },
+    { "SCSI3#0",  0x200000, DEV_VME_D16A24, 0, DevSCSI3Init, 64, DevSCSI3Intr },
     { "SCSI3#1",  0x210000, DEV_VME_D16A24, 1, DevSCSI3Init, 65, DevSCSI3Intr },
     { "SCSI3#2",  0x220000, DEV_VME_D16A24, 2, DevSCSI3Init, 66, DevSCSI3Intr },
     { "SCSI3#3",  0x230000, DEV_VME_D16A24, 3, DevSCSI3Init, 67, DevSCSI3Intr },
