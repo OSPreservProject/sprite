c19043d45d90299e5bb020dd4cd129787912cb12
jhh <>
1991-10-17 13:28:39 +0000

    Initial revision

---
+++ CVSROOT/kernel/mem/ds5000.md/memAsm.s
@@ -0,0 +1,35 @@
+/* memAsm.s --
+ *
+ *	Routine to return callers PC.
+ *
+ *     	This file contains all hardware dependent routines for the PMAX.
+ *
+ * Copyright (C) 1989 by Digital Equipment Corporation, Maynard MA
+ *
+ *			All Rights Reserved
+ *
+ * Permission to use, copy, modify, and distribute this software and its 
+ * documentation for any purpose and without fee is hereby granted, 
+ * provided that the above copyright notice appear in all copies and that
+ * both that copyright notice and this permission notice appear in 
+ * supporting documentation, and that the name of Digital not be
+ * used in advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.  
+ *
+ * Digitial disclaims all warranties with regard to this software, including
+ * all implied warranties of merchantability and fitness.  In no event shall
+ * Digital be liable for any special, indirect or consequential damages or
+ * any damages whatsoever resulting from loss of use, data or profits,
+ * whether in an action of contract, negligence or other tortious action,
+ * arising out of or in connection with the use or performance of this
+ * software.
+ *
+ * rcs = $Header$ SPRITE (DECWRL)
+ */
+
+#include <regdef.h>
+
+    .globl Mem_CallerPC
+Mem_CallerPC:
+    add		v0, zero, zero
+    j		ra
