7f2b5d98d7b08af6fd5acdefa5f1fbcd827091a8
mgbaker <>
1991-11-05 12:20:39 +0000

    Fixing an erroneously checked in previous copy.

--- CVSROOT/kernel/mach/sun4.md/mach.h
+++ CVSROOT/kernel/mach/sun4.md/mach.h
@@ -254,12 +254,4 @@ extern	Address	mach_MaxUserStackAddr;
 extern	int	mach_LastUserStackPage;
 extern	Mach_State  *machCurStatePtr;
 
-/*
- * New fast boot stuff.
- */
-extern	int	storedDataSize;
-extern	char	storedData[];
-extern	char	*mach_RestartTablePtr;
-extern	void	Mach_FastBoot	_ARGS_((void));
-
 #endif /* _MACH */
