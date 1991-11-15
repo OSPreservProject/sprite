82eca5b2abec8370c1a970d7d0c3cb5a04920284
kupfer <>
1991-11-15 20:58:12 +0000

    Add missing RCS Header line.

--- CVSROOT/kernel/net/sun4c.md/netLEMachInt.h
+++ CVSROOT/kernel/net/sun4c.md/netLEMachInt.h
@@ -11,6 +11,8 @@
  * makes no representations about the suitability of this
  * software for any purpose.  It is provided "as is" without
  * express or implied warranty.
+ *
+ * $Header$ SPRITE (Berkeley)
  */
 
 #ifndef _NETLEMACHINT
