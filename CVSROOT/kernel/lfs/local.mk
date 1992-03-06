2ba5527a3b7eccb83809c4bcf6d99ec19bf78049
jhh <>
1992-03-06 15:43:56 +0000

    Turned off optimization until we can track down the problems we've been having.

--- CVSROOT/kernel/lfs/local.mk
+++ CVSROOT/kernel/lfs/local.mk
@@ -2,5 +2,10 @@
 # LFS local.mk file.  
 #
 
+#
+# Turn off optimization until we work out the bugs.
+#
+NOOPTIMIZATION=true
+
 CFLAGS += -DLFS_STATS_COLLECT -DERROR_CHECK 
 #include <$(SYSMAKEFILE)>
