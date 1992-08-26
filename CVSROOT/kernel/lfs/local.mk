8fdd586c99a4d31d2ab772d1c2b30a347c770873
shirriff <>
1992-08-26 21:52:35 +0000

    Changed tags so the file system has a single tags file.

--- CVSROOT/kernel/lfs/local.mk
+++ CVSROOT/kernel/lfs/local.mk
@@ -9,3 +9,9 @@ NOOPTIMIZATION=true
 
 CFLAGS += -DLFS_STATS_COLLECT -DERROR_CHECK -DVERIFY_CLEAN
 #include <$(SYSMAKEFILE)>
+
+CTAGS = true	# i.e. just return success, don't do anything
+
+tags:: $(ALLCSRCS) $(HDRS)
+	/sprite/cmds/ctags -f tags.here $(CTFLAGS) $(ALLCSRCS) $(HDRS)
+	/sprite/src/kernel/fs/fstags
